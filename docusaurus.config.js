b1286b92598ab75c3bb6e9da88fa20470ae411a9
ImmanuelSegol <3ditds@gmail.com>
2024-01-08 10:44:19 +0000

    refactor

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,11 +72,33 @@ const config = {
         respectPrefersColorScheme: false,
       },
       algolia: {
-        appId: 'C4J3NJDLCO',      
-        apiKey: 'e633ae6e113f548d55297a2cca6ae31f',
-        indexName: 'ingonyama',
-        searchPagePath: 'search',
+        // The application ID provided by Algolia
+        appId: 'YOUR_APP_ID',
+  
+        // Public API key: it is safe to commit it
+        apiKey: 'YOUR_SEARCH_API_KEY',
+  
+        indexName: 'YOUR_INDEX_NAME',
+  
+        // Optional: see doc section below
         contextualSearch: true,
+  
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: 'external\\.com|domain\\.com',
+  
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: '/docs/', // or as RegExp: /\/docs\//
+          to: '/',
+        },
+  
+        // Optional: Algolia search parameters
+        searchParameters: {},
+  
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: 'search',
+  
+        //... other Algolia params
       },
       navbar: {
         title: 'Ingonyama Developer Documentaion',
